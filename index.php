85bc0fb99d20ea571bdf6caf9b395d0ba58c12b5
Abi <abifarhan04@gmail.com>
2019-05-09 17:20:00 +0700

    Halaman Awal
    Pembaharuan

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
+<?php
+session_start();
+if (isset($_SESSION['keranjang']) || (!empty($_SESSION['keranjang']))) {
+	$banyak = count($_SESSION['keranjang']);
+}
+
+?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>MugOn | Home</title>
 	<meta charset="UTF-8">
@@ -7,23 +15,24 @@
 	<meta name="keywords" content="divisima, eCommerce, creative, html">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- Favicon -->
-	<link href="img/logo.png" rel="shortcut icon"/>
+	<link href="img/logo.png" rel="shortcut icon" />
 
 	<!-- Google Font -->
 	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
 
 
 	<!-- Stylesheets -->
-	<link rel="stylesheet" href="css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="css/font-awesome.min.css"/>
-	<link rel="stylesheet" href="css/flaticon.css"/>
-	<link rel="stylesheet" href="css/slicknav.min.css"/>
-	<link rel="stylesheet" href="css/jquery-ui.min.css"/>
-	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
-	<link rel="stylesheet" href="css/animate.css"/>
-	<link rel="stylesheet" href="css/style.css"/>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/flaticon.css" />
+	<link rel="stylesheet" href="css/slicknav.min.css" />
+	<link rel="stylesheet" href="css/jquery-ui.min.css" />
+	<link rel="stylesheet" href="css/owl.carousel.min.css" />
+	<link rel="stylesheet" href="css/animate.css" />
+	<link rel="stylesheet" href="css/style.css" />
 
 </head>
+
 <body>
 	<!-- tampilan loading -->
 	<div id="preloder">
@@ -37,31 +46,61 @@
 				<div class="row">
 					<div class="col-lg-2 text-center text-lg-left">
 						<!-- logo -->
-						<a href="./index.html" class="site-logo">
+						<a href="index.php" class="site-logo">
 							<img src="img/logo.png" alt="">
 						</a>
 					</div>
-					<div class="col-xl-6 col-lg-5">
-						<!-- <form class="header-search-form">
-							<input type="text" placeholder="Search on divisima ....">
-							<button><i class="flaticon-search"></i></button>
-						</form> -->
+					<div class="col-lg-3">
+
 					</div>
-					<div class="col-xl-4 col-lg-5">
+					<div class="col-lg-4">
 						<div class="user-panel">
 							<div class="up-item">
 								<i class="flaticon-profile"></i>
-								<a href="login.php">Login</a> atau <a href="daftar.php">Daftar</a>
+								<?php if (isset($_SESSION['pembeli'])) { ?>
+									<a href="profil.php"><?php echo $_SESSION['pembeli']['nama_pembeli']; ?></a>
+								<?php } else { ?>
+									<a href="login.php">Login</a> atau <a href="daftar.php">Daftar</a>
+								<?php } ?>
 							</div>
 							<div class="up-item">
 								<div class="shopping-card">
 									<i class="flaticon-bag"></i>
-									<span>0</span>
+									<span>
+										<?php if (isset($_SESSION['keranjang']) || (!empty($_SESSION['keranjang']))) {
+											echo $banyak;
+										} else {
+											echo '0';
+										} ?>
+									</span>
+
 								</div>
 								<a href="keranjang.php">Keranjang Belanja</a>
 							</div>
 						</div>
 					</div>
+					<div class="col-lg-3">
+						<form class="form-inline mr-auto" method="post" action="">
+							<input class="form-control mr-sm-2" name="cari" type="text" placeholder="Cari Ikan" aria-label="Search">
+							<button class="btn btn-outline-secondary btn-rounded my-0" name="search" type="submit">Cari</button>
+						</form>
+					</div>
+
+					<!-- fungsi search ikan -->
+					<!-- ini fungsi untuk mencari ikan -->
+					<?php
+					if (isset($_POST['cari'])) {
+						$koneksi = new mysqli("localhost", "root", "", "mugon");
+						$ambil = $koneksi->query("SELECT id_ikan FROM ikan WHERE nama_ikan LIKE '%$_POST[cari]%' ");
+						$pecah = $ambil->fetch_assoc();
+						if (!empty($pecah)) {
+							echo "<script>location='detail_ikan.php?id=" . $pecah['id_ikan'] . "'</script>";
+						} else {
+							echo "<script>alert('Ikan yang anda cari tidak ada !!');</script>";
+						}
+					}
+					?>
+
 				</div>
 			</div>
 		</div>
@@ -70,6 +109,7 @@
 				<!-- bagian menu/ navigasi -->
 				<ul class="main-menu">
 					<li><a href="index.php">Home</a></li>
+					<li><a href="riwayat.php">Riwayat</a></li>
 					<li><a href="tentang.php">MungOn?</a></li>
 					<li><a href="metode.php">Metode Transaksi</a></li>
 					<li><a href="kontak.php">Kontak</a></li>
@@ -79,50 +119,54 @@
 	</header>
 	<!-- akhir bagian dari kepala/atas -->
 
-
-
-	<?php 
+	<?php
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$dbname = "mugon";
 	//create connection
 	$ikan = mysqli_connect($servername, $username, $password, $dbname);
-	if ($ikan->connect_error){
-		die("Connection failed:".$ikan->connect_error);
+	if ($ikan->connect_error) {
+		die("Connection failed:" . $ikan->connect_error);
 	}
 
 	$sql = "SELECT * FROM ikan";
-	$tampilIkan = mysqli_query($ikan,$sql);
+	$tampilIkan = mysqli_query($ikan, $sql);
 	?>
 	<!-- bagian halaman seluncur/bergeser -->
 	<section class="hero-section">
 		<div class="hero-slider owl-carousel">
 			<?php
 			$batas = mysqli_num_rows($tampilIkan);
-			while($variabel = mysqli_fetch_assoc($tampilIkan)) {?>
-			<?php if (!($batas>4)){?>
-			<div class="hs-item set-bg" data-setbg="img/Ikan/<?php echo $variabel['gambar_ikan'];?>">
-				<div class="container">
-					<div class="row">
-						<div class="col-xl-6 col-lg-7 text-white">
-							<span>Ikan Terbaru</span>
-							<?php echo "<h2>".$variabel ['nama_ikan']."</h2>"?>
-							<p>Ikan Tuna masih segar, ayo dibeli jangan sampai kehabisan, sisa stok hanya 10kg.</p>
-							<a href="detail_ikan.php?id=<?php echo $variabel ['id_ikan'] ?>" class="site-btn sb-line">Lihat</a>
-							<a href="beli.php?id=<?php echo $variabel['id_ikan']?>" class="site-btn sb-white">Beli Sekarang</a>
-						</div>	
+			while ($variabel = mysqli_fetch_assoc($tampilIkan)) { ?>
+				<?php if (!($batas > 4)) { ?>
+					<div class="hs-item set-bg" data-setbg="img/Ikan/<?php echo $variabel['gambar_ikan']; ?>">
+						<div class="container">
+							<div class="row">
+								<div class="col-xl-6 col-lg-7 text-white">
+									<span>Ikan Terbaru</span>
+									<?php echo "<h2>" . $variabel['nama_ikan'] . "</h2>" ?>
+									<p><?php echo $variabel['keterangan'] ?></p>
+									<a href="detail_ikan.php?id=<?php echo $variabel['id_ikan'] ?>" class="site-btn sb-line">Lihat</a>
+									<a href="beli.php?id=<?php echo $variabel['id_ikan'] ?>" class="site-btn sb-white">Beli Sekarang</a>
+								</div>
+							</div>
+							<div class="offer-card text-white">
+								<?php if ($variabel['stok_ikan'] <= 0) { ?>
+
+									<h3>SUDAH HABIS</h3>
+								<?php } else { ?>
+									<span>Hanya</span>
+									<?php echo "<h4>" . $variabel['harga_ikan'] . "/kg</h4>" ?>
+									<p>Beli Sekarang</p>
+								<?php } ?>
+							</div>
+						</div>
 					</div>
-					<div class="offer-card text-white">
-						<span>Hanya</span>
-						<?php echo "<h4>". $variabel['harga_ikan']."/kg</h4>"?>
-						<p>Beli Sekarang</p>
-					</div>
-				</div>
-			</div>
-			<?php } ?>
-			<?php $batas--;}?>
-			</div>
+				<?php } ?>
+				<?php $batas--;
+			} ?>
+		</div>
 		<div class="container">
 			<div class="slide-num-holder" id="snh-1"></div>
 		</div>
@@ -171,27 +215,32 @@
 				<h2>Produk Terbaru</h2>
 			</div>
 			<div class="product-slider owl-carousel">
-			<?php 	
+				<?php
 				$sql = "SELECT * FROM ikan";
-				$tampilIkan = mysqli_query($ikan,$sql);
-				$batas = mysqli_num_rows($tampilIkan);
-				while($variabel = mysqli_fetch_assoc($tampilIkan)){ ?>
-				<?php if (!($batas > 6)) { ?>
-				<div class="product-item">
-					<div class="pi-pic">
-						<div class="tag-sale">STOK: <?php echo $variabel['stok_ikan']; ?>Kg</div>		
-						<a href="detail_ikan.php?id=<?php echo $variabel['id_ikan']?>"><img src="img/Ikan/<?php echo $variabel['gambar_ikan'];?>"alt="tampil"></a>
-						<div class="pi-links">
-							<a href="beli.php?id=<?php echo $variabel['id_ikan']?>" class="add-card"><i class="flaticon-bag"></i><span>Beli Sekarang</span></a>
+				$tampilIkan = mysqli_query($ikan, $sql);
+				$batas = mysqli_num_rows($tampilIkan);						//membatasi 
+				while ($variabel = mysqli_fetch_assoc($tampilIkan)) { ?>
+					<?php if (!($batas > 6)) { ?>
+						<div class="product-item">
+							<div class="pi-pic">
+								<?php if ($variabel['stok_ikan'] <= 0) { ?>
+									<div class="tag-sale">HABIS</div>
+								<?php } else { ?>
+									<div class="tag-sale">STOK: <?php echo $variabel['stok_ikan']; ?>kg</div>
+								<?php } ?>
+								<a href="detail_ikan.php?id=<?php echo $variabel['id_ikan'] ?>"><img src="img/Ikan/<?php echo $variabel['gambar_ikan']; ?>" alt="tampil"></a>
+								<div class="pi-links">
+									<a href="beli.php?id=<?php echo $variabel['id_ikan'] ?>" class="add-card"><i class="flaticon-bag"></i><span>Beli Sekarang</span></a>
+								</div>
+							</div>
+							<div class="pi-text">
+								<?php echo "<h6>" . $variabel['harga_ikan'] . "/kg</h6>" ?>
+								<?php echo "<p>" . $variabel['nama_ikan'] . "</p>" ?>
+							</div>
 						</div>
-					</div>
-					<div class="pi-text">
-						<?php echo "<h6>". $variabel['harga_ikan']."/kg</h6>"?>
-						<?php echo "<p>". $variabel['nama_ikan']."/kg</p>"?>
-					</div>
-				</div>
-			<?php }?>
-			<?php $batas--; }?>
+					<?php } ?>
+					<?php $batas--;
+				} ?>
 			</div>
 		</div>
 	</section>
@@ -205,27 +254,31 @@
 				<h2>IKAN HARI INI</h2>
 			</div>
 			<div class="row">
-				<?php	
+				<?php
 				$sql = "SELECT * FROM ikan";
-				$tampilIkan = mysqli_query($ikan,$sql);
-				while($variabel = mysqli_fetch_assoc($tampilIkan)){ ?>
-				<div class="col-lg-3 col-sm-6">
-					<div class="product-item">
-						<div class="pi-pic">
-							<div class="tag-sale">STOK: <?php echo $variabel['stok_ikan']; ?>Kg</div>	
-							<a href="detail_ikan.php?id=<?php echo $variabel['id_ikan']?>"><img src="img/Ikan/<?php echo $variabel['gambar_ikan'];?>" alt=""></a>
-							<div class="pi-links">
-								<a href="beli.php?id=<?php echo $variabel['id_ikan']?>" class="add-card"><i class="flaticon-bag"></i><span>Beli Sekarang</span></a>
+				$tampilIkan = mysqli_query($ikan, $sql);
+				while ($variabel = mysqli_fetch_assoc($tampilIkan)) { ?>
+					<div class="col-lg-3 col-sm-6">
+						<div class="product-item">
+							<div class="pi-pic">
+								<?php if ($variabel['stok_ikan'] <= 0) { ?>
+									<div class="tag-sale">HABIS</div>
+								<?php } else { ?>
+									<div class="tag-sale">STOK: <?php echo $variabel['stok_ikan']; ?>kg</div>
+								<?php } ?>
+								<a href="detail_ikan.php?id=<?php echo $variabel['id_ikan'] ?>"><img src="img/Ikan/<?php echo $variabel['gambar_ikan']; ?>" alt=""></a>
+								<div class="pi-links">
+									<a href="beli.php?id=<?php echo $variabel['id_ikan'] ?>" class="add-card"><i class="flaticon-bag"></i><span>Beli Sekarang</span></a>
+								</div>
+							</div>
+							<div class="pi-text">
+								<?php echo "<h6>" . $variabel['harga_ikan'] . "/kg</h6>" ?>
+								<?php echo "<p>" . $variabel['nama_ikan'] . "</p>" ?>
 							</div>
 						</div>
-						<div class="pi-text">
-							<?php echo "<h6>". $variabel['harga_ikan']."/kg</h6>"?>
-							<?php echo "<p>". $variabel['nama_ikan']."</p>"?>
-						</div>
 					</div>
-				</div>
-				<?php }?>
-		</div>
+				<?php } ?>
+			</div>
 	</section>
 	<!-- akhir bagian ikan hari ini -->
 
@@ -248,7 +301,7 @@
 	<section class="footer-section">
 		<div class="container">
 			<div class="footer-logo text-center">
-				<a href="index.html"><img class="site-logo" src="./img/logo.png" alt=""></a>
+				<a href="index.php"><img class="site-logo" src="img/logo.png" alt=""></a>
 			</div>
 			<div class="row">
 				<div class="col-lg-3 col-sm-6">
@@ -259,6 +312,7 @@
 				</div>
 				<div class="col-lg-3 col-sm-6">
 				</div>
+
 				<div class="col-lg-3 col-sm-6">
 					<div class="footer-widget contact-widget">
 						<h2>Questions</h2>
@@ -291,7 +345,9 @@
 					<a href="" class="youtube"><i class="fa fa-youtube"></i><span>youtube</span></a>
 				</div>
 
-<p class="text-white text-center mt-5">Copyright &copy;<script>document.write(new Date().getFullYear());</script> MugeeOnline <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">MugOn</a></p>
+				<p class="text-white text-center mt-5">Copyright &copy;<script>
+						document.write(new Date().getFullYear());
+					</script> MugeeOnline <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">MugOn</a></p>
 
 			</div>
 		</div>
@@ -310,6 +366,6 @@
 	<script src="js/jquery-ui.min.js"></script>
 	<script src="js/main.js"></script>
 
-	</body>
-</html>
+</body>
 
+</html>
