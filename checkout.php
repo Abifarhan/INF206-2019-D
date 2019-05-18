607dc269e5e38288e5afcfe9db72dd05a3dd93f5
khairul <khairulr59@gmail.com>
2019-05-18 22:36:12 +0700

    fungsionalitas checkout(belum selesai)
    baru selesai di dihalamn checkout dan belum terhubung ke database dan belum dapat berfungsi.

---
+++ checkout.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MugOn | Checkout</title>
+	<meta charset="UTF-8">
+	<meta name="description" content=" Divisima | eCommerce Template">
+	<meta name="keywords" content="divisima, eCommerce, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="img/logo.png" rel="shortcut icon"/>
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
+
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/flaticon.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+	<link rel="stylesheet" href="css/jquery-ui.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+</head>
+<body>
+	<!-- tampilan loading -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- bagian atas/kepala -->
+	<header class="header-section">
+		<div class="header-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-2 text-center text-lg-left">
+						<!-- logo -->
+						<a href="index.php" class="site-logo">
+							<img src="img/logo.png" alt="">
+						</a>
+					</div>
+					<div class="col-lg-3">
+
+					</div>
+					<div class="col-lg-4">
+						<div class="user-panel">
+							<div class="up-item">
+								<i class="flaticon-profile"></i>
+									<a href="login.php">Login</a> atau <a href="daftar.php">Daftar</a>
+								
+							</div>
+							<div class="up-item">
+								<div class="shopping-card">
+									<i class="flaticon-bag"></i>
+									<span>
+									</span>
+
+								</div>
+								<a href="keranjang.php">Keranjang Belanja</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3">
+						<form class="form-inline mr-auto" method="post" action="">
+						  <input class="form-control mr-sm-2" name="cari" type="text" placeholder="Cari Ikan" aria-label="Search">
+						  <button class="btn btn-outline-secondary btn-rounded my-0" name="search" type="submit">Cari</button>
+						</form>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<nav class="main-navbar text-center">
+			<div class="container">
+				<!-- bagian menu/ navigasi -->
+				<ul class="main-menu">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="riwayat.php">Riwayat</a></li>
+					<li><a href="tentang.php">MungOn?</a></li>
+					<li><a href="metode.php">Metode Transaksi</a></li>
+					<li><a href="kontak.php">Kontak</a></li>
+				</ul>
+			</div>
+		</nav>
+	</header>
+	<!-- akhir bagian dari kepala/atas -->
+
+
+	<!-- Bagian info -->
+	<div class="page-top-info">
+		<div class="container">
+			<h4>Pemesanan</h4>
+			<div class="site-pagination">
+				<a href="index.php">Home</a> /
+				<a href="">Pemesanan</a>
+			</div>
+		</div>
+	</div>
+	<!-- akhir bagian info -->
+
+
+	<!-- checkout section  -->
+	<section class="checkout-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 order-2 order-lg-1">
+					<form class="checkout-form" method="post">
+						<div class="cf-title">Alamat Pengiriman</div>
+						<div class="row">
+							<div class="col-md-7">
+								<p>Informasi Pengiriman</p>
+							</div>
+							<div class="col-md-5">
+								<div class="cf-radio-btns address-rb">
+									<div class="cfr-item">
+										<input type="radio" name="pilih" id="one" value="alamat_saya">
+										<label for="one">pakai alamat Saya</label>
+									</div>
+									<div class="cfr-item">
+										<input type="radio" name="pilih" id="two" value="alamat_lain">
+										<label for="two">Pakai Alamat lain </label>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
+						<div class="row address-inputs">
+							<div class="col-md-12">
+								<label>Alamat Saya</label>
+								<input type="text" name="alamat_saya" value="" readonly>
+								<input type="text" name="alamat_lain" placeholder="Alamat Lainnya">
+								<input type="text" name="kota" placeholder="Kota">
+							</div>
+							<div class="col-md-6">
+								<input type="text" name="kd_pos" placeholder="Kode Pos">
+							</div>
+							<div class="col-md-6">
+								<input type="text" name="no_hp" value="" readonly>
+							</div>
+						</div>
+						<div class="cf-title">Info Cara Pengiriman</div>
+						<div class="row shipping-btns">
+							<div class="col-12">
+							<h4>Pengiriman Gratis</h4> <br>
+							<h5>Pengiriman akan dilakukan secepatnya setelah anda melakukan checkout pemesanan anda</h5>
+							</div>
+						</div>
+						<div class="cf-title">Pembayaran</div>
+						<h6>Pembayaran dilakukan secara COD(Cast On Delivery), yaitu anda dapat melakukan pembayaran ketika pesanan ikan anda telah anda terima</h6>
+						<button name="checkout" class="site-btn submit-order-btn">Lakukan Pemesanan</button>
+					</form>
+				</div>
+
+				<div class="col-lg-4 order-1 order-lg-2">
+					<div class="checkout-cart">
+						<h3>Pesanan Anda</h3>
+						<ul class="product-list">
+
+
+							
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- checkout section end -->
+
+	<!-- bagian bawah -->
+	<section class="footer-section">
+		<div class="container">
+			<div class="footer-logo text-center">
+				<a href="index.php"><img class="site-logo" src="./img/logo.png" alt=""></a>
+			</div>
+			<div class="row">
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget about-widget">
+						<h2>About</h2>
+						<p>MugOn adalah aplikasi tempat menjual ikan-ikan segar yang akan langsung diantar kerumah pembeli, dan pembayaran dilakukan ditempat.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+				</div>
+	
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget contact-widget">
+						<h2>Questions</h2>
+						<div class="con-info">
+							<span>C.</span>
+							<p>MugOn Company</p>
+						</div>
+						<div class="con-info">
+							<span>B.</span>
+							<p>Lab Terpadu Unsyiah</p>
+						</div>
+						<div class="con-info">
+							<span>T.</span>
+							<p>+62 923 2352 2342</p>
+						</div>
+						<div class="con-info">
+							<span>E.</span>
+							<p>MugOn@gmail.com</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="social-links-warp text-center">
+			<div class="container">
+				<div class="social-links">
+					<a href="" class="instagram"><i class="fa fa-instagram"></i><span>instagram</span></a>
+					<a href="" class="facebook"><i class="fa fa-facebook"></i><span>facebook</span></a>
+					<a href="" class="twitter"><i class="fa fa-twitter"></i><span>twitter</span></a>
+					<a href="" class="youtube"><i class="fa fa-youtube"></i><span>youtube</span></a>
+				</div>
+
+<p class="text-white text-center mt-5">Copyright &copy;<script>document.write(new Date().getFullYear());</script> MugeeOnline <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">MugOn</a></p>
+
+			</div>
+		</div>
+	</section>
+	<!-- akhir bagian akhir-->
+
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery.nicescroll.min.js"></script>
+	<script src="js/jquery.zoom.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
