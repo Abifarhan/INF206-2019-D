207feee1213a9e8186ca253de566cb94286c0a48
raisyahsn <raisyahusna25@gmail.com>
2019-05-29 02:39:37 +0700

    Fungsi dari logout pembeli (selesai)
    coude beserta dengan comment

--- logout.php
+++ logout.php
@@ -1,5 +1,9 @@
+<!--Fungsi sudah terjadi Logout pada pembeli
+@author : Raisya Husna Agustin
+-->
+
 <?php
-	session_start();
+	session_start(); // untuk menghapus session 
 	session_destroy();
 	unset($_SESSION["user"]);
 	echo "<script>alert('anda telah logout');</script>";
